08055ddc62356fa49bf9616cbe2c40b5d0057f65
Sahas Belbase <belbase.sahas@gmail.com>
2024-01-16 17:18:37 +0545

    Initial commit

---
+++ src/app/property/authentication/authentication.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authentication',
+  template :'This is sahas',
+  standalone: true,
+  imports: [],
+  templateUrl: './authentication.component.html',
+  styleUrl: './authentication.component.css'
+})
+export class AuthenticationComponent {
+
+}
